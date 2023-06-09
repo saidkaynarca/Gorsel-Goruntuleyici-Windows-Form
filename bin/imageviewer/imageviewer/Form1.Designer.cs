29f1dd3148bf4551aad9d5c0290fc4956c8db3e6
Said Kaynarca <103364823+saidkaynarca@users.noreply.github.com>
2023-06-10 01:14:23 +0300

    Add files via upload

---
+++ bin/imageviewer/imageviewer/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace imageviewer
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+      
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+       
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.stretchChecklist = new System.Windows.Forms.CheckBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.backgroundButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+        
+
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(886, 514);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            
+
+            this.stretchChecklist.AutoSize = true;
+            this.stretchChecklist.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stretchChecklist.Location = new System.Drawing.Point(30, 539);
+            this.stretchChecklist.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.stretchChecklist.Name = "stretchChecklist";
+            this.stretchChecklist.Size = new System.Drawing.Size(70, 21);
+            this.stretchChecklist.TabIndex = 1;
+            this.stretchChecklist.Text = "Resmi Kaldır/Aktif Et";
+            this.stretchChecklist.UseVisualStyleBackColor = true;
+            this.stretchChecklist.CheckedChanged += new System.EventHandler(this.stretchChecklist_CheckedChanged);
+      
+
+            this.showButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showButton.Location = new System.Drawing.Point(254, 526);
+            this.showButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(93, 43);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Resim ";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+       
+
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(387, 526);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(96, 43);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Sil";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+     
+
+            this.backgroundButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backgroundButton.Location = new System.Drawing.Point(518, 526);
+            this.backgroundButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.backgroundButton.Name = "backgroundButton";
+            this.backgroundButton.Size = new System.Drawing.Size(132, 43);
+            this.backgroundButton.TabIndex = 4;
+            this.backgroundButton.Text = "Arka plan";
+            this.backgroundButton.UseVisualStyleBackColor = true;
+            this.backgroundButton.Click += new System.EventHandler(this.backgroundButton_Click);
+    
+
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButton.Location = new System.Drawing.Point(682, 526);
+            this.closeButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(109, 43);
+            this.closeButton.TabIndex = 5;
+            this.closeButton.Text = "Çıkış";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+   
+
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = " JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All fil" +
+                "es (*.*)|*.*";
+            this.openFileDialog1.Title = "Select a picture file";
+        
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 589);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.backgroundButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.stretchChecklist);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "RESİM GÖRÜNTÜLEYİCİ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox stretchChecklist;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button backgroundButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+
+    }
+}
+
